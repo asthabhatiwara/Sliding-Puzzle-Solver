e50344ed1694f6e3e3ab466acf62b6af614702f5
asthabhatiwara <62290422+asthabhatiwara@users.noreply.github.com>
2021-07-03 12:57:47 +0530

    Update Check_Solvable.cpp

--- Check_Solvable.cpp
+++ Check_Solvable.cpp
@@ -1,3 +1,6 @@
+/****
+ *  check Solvability 
+ ***/
 
 
 int merge(int a[n*n-1] , int start,int mid,int end){
@@ -32,7 +35,7 @@ int merge(int a[n*n-1] , int start,int mid,int end){
 
 }
 
-
+//count inversion pairs
 int mergesort(int start, int end , int a[n*n-1]){
     if(start>=end){
         return 0;
@@ -50,6 +53,8 @@ int mergesort(int start, int end , int a[n*n-1]){
 
 }
 
+
+//find the pos of blank row from bottom
 int find_blankRow_pos(){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -62,6 +67,7 @@ int find_blankRow_pos(){
     return -1;
 }
 
+//check if given puzzle is solvable or not
 bool isSolvable(){
     int a[n*n];
     int p=0;
@@ -74,22 +80,25 @@ bool isSolvable(){
             a[p++] = initial[i][j];
         }
     }
-    int inv_cnt=0;
+    int cnt=0;
     int x = n*n-1;
-    int cnt = mergesort(0,x-1,a);
+    int inv_cnt = mergesort(0,x-1,a);   //count inversion in n^2logn time complexity 
     int pos = find_blankRow_pos();
 
+    
+    //if n is odd then #of inversion should be even
     if(n&1){
-        if(cnt&1){
+        if(inv_cnt&1){
             return 0;
         }
         else{
                return 1;
         }
     }
-
+     
+    // if n is even then (#inversions even) == (blank on odd row from bottom)
     else{
-        int flag =pos+cnt;
+        int flag =pos+inv_cnt;
         if(flag&1){
                return 1;
         }
